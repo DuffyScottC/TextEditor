033c87bc8438e5b47605bbe540f79907e8d1c137
Scott Duffy <duffyscottc@gmail.com>
2017-10-20 20:00:22 -0400

    imported autogenerated text to Controller class

--- src/texteditor/Controller.java
+++ src/texteditor/Controller.java
@@ -3,12 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package texteditor;
 
 /**
  *
  * @author Scott
  */
+
+package texteditor;
+
+import views.MyFrame;
+
 public class Controller {
-    
+
+  private final MyFrame frame = new MyFrame();
+  
+  public Controller() {
+    frame.setTitle( getClass().getSimpleName() );
+    frame.setLocationRelativeTo(null);
+    // you can adjust the size with something like this:
+    // frame.setSize(600, 500);
+	
+    // event handlers
+  }
+
+  public static void main(String[] args) {
+    Controller app = new Controller();
+    app.frame.setVisible(true);
+  }
 }
