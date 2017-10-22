7e6da95fd5da39ddacc008cca7bb57fa32d15a42
Scott Duffy <duffyscottc@gmail.com>
2017-10-22 19:49:50 -0400

    enable menu item save and save as on open

--- src/texteditor/Controller.java
+++ src/texteditor/Controller.java
@@ -28,15 +28,24 @@ public class Controller {
     // frame.setSize(600, 500);
     JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
     
+    //Get the varius graphical elements
     JMenuItem openMenuItem = frame.getOpenMenuItem();
+    JMenuItem saveMenuItem = frame.getSaveMenuItem();
+    JMenuItem saveAsMenuItem = frame.getSaveAsMenuItem();
+    
     openMenuItem.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             
+            //use a file chooser to open the file the user wants
             chooser.setDialogTitle("Open");
             if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) { //if the user chose a valid file
                 
                 System.out.println(chooser.getSelectedFile().getAbsolutePath());
+                
+                //If the user opens a file, then the other two options (save and save as) should be enabled
+                saveMenuItem.setEnabled(true);
+                saveAsMenuItem.setEnabled(true);
                 
             }
             
