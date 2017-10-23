ca2111b443321661dfeb5c0971100e77842c302d
Scott Duffy <duffyscottc@gmail.com>
2017-10-22 20:10:19 -0400

    added code to read in file

--- src/texteditor/Controller.java
+++ src/texteditor/Controller.java
@@ -3,60 +3,82 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Scott
  */
-
 package texteditor;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
 import views.MyFrame;
 
 public class Controller {
 
-  private final MyFrame frame = new MyFrame();
-  
-  public Controller() {
-    frame.setTitle( getClass().getSimpleName() );
-    frame.setLocationRelativeTo(null);
-    // you can adjust the size with something like this:
-    // frame.setSize(600, 500);
-    JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
-    
-    //Get the varius graphical elements
-    JMenuItem openMenuItem = frame.getOpenMenuItem();
-    JMenuItem saveMenuItem = frame.getSaveMenuItem();
-    JMenuItem saveAsMenuItem = frame.getSaveAsMenuItem();
-    
-    openMenuItem.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            
-            //use a file chooser to open the file the user wants
-            chooser.setDialogTitle("Open");
-            if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) { //if the user chose a valid file
-                
-                System.out.println(chooser.getSelectedFile().getAbsolutePath());
-                
-                //If the user opens a file, then the other two options (save and save as) should be enabled
-                saveMenuItem.setEnabled(true);
-                saveAsMenuItem.setEnabled(true);
-                
-            }
-            
-        }
-    });
-	
-    // event handlers
-  }
+    private final MyFrame frame = new MyFrame();
+    private File file; //the file that holds the text the user is editing
 
-  public static void main(String[] args) {
-    Controller app = new Controller();
-    app.frame.setVisible(true);
-  }
+    public Controller() {
+        frame.setTitle(getClass().getSimpleName());
+        frame.setLocationRelativeTo(null);
+        // you can adjust the size with something like this:
+        // frame.setSize(600, 500);
+        JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+
+        //Get the varius graphical elements
+        JMenuItem openMenuItem = frame.getOpenMenuItem();
+        JMenuItem saveMenuItem = frame.getSaveMenuItem();
+        JMenuItem saveAsMenuItem = frame.getSaveAsMenuItem();
+
+        openMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //use a file chooser to open the file the user wants
+                chooser.setDialogTitle("Open");
+                if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) { //if the user chose a valid file
+                    
+                    try {
+                        //Create a path object from chooser
+                        Path path = chooser.getSelectedFile().toPath();
+                        System.out.println(path); //print the path for debugging purposes
+                        file = path.toFile(); //instantiate the file object
+                        
+                        String content = new String(Files.readAllBytes(path)); //this may throw an exception
+                        
+                        
+                        
+                        //If the user opens a file, then the other two options (save and save as) should be enabled
+                        saveMenuItem.setEnabled(true);
+                        saveAsMenuItem.setEnabled(true);
+                        
+                    } catch (IOException ex) { //catch any potential errors
+                        
+                        System.out.println("Unable to read selected file");
+                        System.err.println(ex);
+                        
+                    }
+
+                }
+
+            }
+        });
+
+        // event handlers
+    }
+
+    public static void main(String[] args) {
+        Controller app = new Controller();
+        app.frame.setVisible(true);
+    }
 }
