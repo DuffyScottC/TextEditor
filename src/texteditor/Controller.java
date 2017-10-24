ef9e2cec14d40ceab871d271383d22021d33e1ed
Scott Duffy <duffyscottc@gmail.com>
2017-10-23 23:19:45 -0400

    checks if user wants to continue if open or new without saveing

--- src/texteditor/Controller.java
+++ src/texteditor/Controller.java
@@ -20,6 +20,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import views.MyFrame;
@@ -38,7 +39,7 @@ public class Controller {
         // you can adjust the size with something like this:
         // frame.setSize(600, 500);
         JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
-        
+
         //Get the varius graphical elements
         JMenuItem openMenuItem = frame.getOpenMenuItem();
         JMenuItem saveMenuItem = frame.getSaveMenuItem();
@@ -49,33 +50,41 @@ public class Controller {
         editedTextField = frame.getEditedTextField(); //instantiate the class member editedTextField
         editedTextField.setMinimumSize(new Dimension(500, editedTextField.getHeight()));
         modified = false;
-        
+
         newMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                if (!shouldContinue()) { //if the user does not want to continue
+                    return;
+                }
                 
                 //clear the text from the textArea
                 textArea.setText("");
-                
+
                 //replace the display name
                 fileNameTextField.setText("<NEW FILE>");
-                
+
                 //disable the save menu item (if neccessary)
                 saveMenuItem.setEnabled(false);
-                
+
                 //enable the save as menu item (if neccessary)
                 saveAsMenuItem.setEnabled(true);
-                
+
                 //enable the text area (if neccessary)
                 textArea.setEditable(true); //enable the textArea (if neccessary)
-                
+
             }
         });
-        
+
         openMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                
+                if (!shouldContinue()) { //if the user does not want to continue
+                    return;
+                }
+                
                 //use a file chooser to open the file the user wants
                 chooser.setDialogTitle("Open");
                 if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) { //if the user chose a valid file
@@ -94,9 +103,8 @@ public class Controller {
                         saveAsMenuItem.setEnabled(true);
 
                         textArea.setEditable(true); //enable the textArea (if neccessary)
-                        
+
                         //put the file name into the fileNameTextField
-                        
                         String workingDirStr = System.getProperty("user.dir"); //get the working directory
                         Path workingDir = Paths.get(workingDirStr); //convert the working directory to a path object
                         Path relativePath = workingDir.relativize(path); //get the relative path of the file from the working directory
@@ -120,7 +128,7 @@ public class Controller {
             public void actionPerformed(ActionEvent e) {
 
                 saveFile(); //save the file
-                
+
                 modified = false; //the file is no longer modified
 
             }
@@ -135,29 +143,29 @@ public class Controller {
                 if (chooserResult == JFileChooser.APPROVE_OPTION) { //if the user successfully saved the file
 
                     path = chooser.getSelectedFile().toPath(); //get the new path to the file save location
-                    
+
                     //check if the file has an extension already
                     String fileName = path.getFileName().toString(); //the name of the file, including the extension
                     if (!fileName.matches(".*\\.\\w+")) { //if the file name does NOT have an extension, then we need to add a .txt
-                        
+
                         //add .txt
                         String fileNameWithExtension = path.getFileName() + ".txt"; //append the .txt extension to the file name
                         //use the resolveSibling method to change the old, extensionless file name to the new filename created above
                         path = path.resolveSibling(fileNameWithExtension); //e.g. this will replace "curdir/sample2" with "curdir/sample2.txt"
-                        
+
                     }
-                    
+
                     //put the file name into the fileNameTextField
                     fileNameTextField.setText(path.getFileName().toString());
-                    
+
                     saveFile(); //save the file
-                    
+
                     modified = false; //the file is no longer modified
-                    
+
                     saveMenuItem.setEnabled(true); //enable the save menu item (if necessary)
-                    
+
                 }
-                
+
             }
         });
 
@@ -189,19 +197,46 @@ public class Controller {
     }
     
     /**
-     * Convenience function that saves the text within textArea to the current path (and removes the edited mark)
+     * Convenience method that asks a user if they want to continue if they try to 
+     * 1) open a file without saving
+     * 2) create a new file without saving
+     * 3) close the program without saving
+     * 
+     * @return true if the process should continue, false if you should return out of the method
+     */
+    private boolean shouldContinue() {
+        if (modified) { //if the file has been modified, but not saved
+
+            int selection = JOptionPane.showConfirmDialog(frame, "OK to overwrite existing file?"); //ask the user if they want to continue
+
+            if (selection != JOptionPane.YES_OPTION) { //if the user did not choose "yes"
+                return false; //cancel the operation
+            }
+            
+            //if the user did choose yes, then we should continue the operation
+
+        } 
+        
+        //if the file has been saved, then we can just return true
+        return true;
+        
+    }
+
+    /**
+     * Convenience function that saves the text within textArea to the current
+     * path (and removes the edited mark)
      */
     private void saveFile() {
         try {
-            
+
             String textAreaBody = textArea.getText(); //get the text stored within the body of the text area
-            
+
             Files.write(path, textAreaBody.getBytes()); //save the body of the text area to the path last selected by the user
-            
+
             System.out.println("file saved to: " + path);
-            
+
             editedTextField.setText(""); //remove the * from the edited text field
-            
+
         } catch (IOException ex) {
 
             System.out.println("Unable to save file");
@@ -209,7 +244,7 @@ public class Controller {
 
         }
     }
-    
+
     public static void main(String[] args) {
         Controller app = new Controller();
         app.frame.setVisible(true);
